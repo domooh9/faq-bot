f39186dbb2b426fabbb218a00ffc78216c595f79
Domnic Omondi <domooh9@gmail.com>
2022-09-09 21:30:45 +0300

    frontend

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h2>FAQ application</h2>
+     <form>
+      <button className='btn'>start</button>
+       <input className= "inpt" placeholder='Enter question...'/>
+      <button className='btm'>send</button>
+     </form>
+    </div>
+  );
+}
+
+export default App;
